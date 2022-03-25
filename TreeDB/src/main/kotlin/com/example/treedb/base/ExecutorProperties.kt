6739567c0d47dbf9058a2b05c1ea2547cc124fc0
MarcinPC <marcin.bobinski9992@gmail.com>
2022-03-25 15:29:31 +0100

    TreeDB delete

--- TreeDB/src/main/kotlin/com/example/treedb/base/ExecutorProperties.kt
+++
@@ -1,9 +0,0 @@
-package com.example.treedb.base
-
-import org.springframework.boot.context.properties.ConstructorBinding
-
-@ConstructorBinding
-data class ExecutorProperties(
-    val corePoolSize: Int,
-    val maxPoolSize: Int
-)
